14952104dda711e309d94f97aca5fd4b4d789c54
Brady Bentley <bradybentley@bradys-macbook-pro.local>
2019-01-08 13:41:05 -0700

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Brady Bentley on 1/8/19.
+//  Copyright © 2019 Brady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
